ea705d19f438013ef1ab1cfde07f8ccefaa56829
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2024-01-16 17:30:08 +0100

    Added login screen

--- secure-notes-frontend/src/app/app.routes.ts
+++ secure-notes-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'notes',
+    loadComponent: () =>
+      import('./features/note/note.component').then((c) => c.NoteComponent),
+  },
+  // { path: '**', redirectTo: '' },
+];
